f82a9f1415d77461d8e9c8eb0c8496ddea1ef9e4
nanjingchj <nanjingchj@gmail.com>
2019-12-13 17:17:07 -0500

    rsa modified

--- src/jmessenger/shared/RegistrationMessage.java
+++ src/jmessenger/shared/RegistrationMessage.java
@@ -1,8 +1,4 @@
 /*
- * Copied and modified from https://www.devglan.com/java8/rsa-encryption-decryption-java
- */
-
- /*
  * The MIT License
  *
  * Copyright 2019 frche1699.
@@ -25,38 +21,35 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+
 package jmessenger.shared;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.security.*;
 import org.jetbrains.annotations.NotNull;
 
+import java.security.PublicKey;
+
 /**
- *
  * @author frche1699
  */
-public class RSAKeyPairGenerator {
+public class RegistrationMessage extends ServerMessage {
+    private PublicKey publicKey;
 
-    private final PrivateKey privateKey;
-    private final PublicKey publicKey;
-
-    public RSAKeyPairGenerator() throws NoSuchAlgorithmException {
-        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
-        keyGen.initialize(1024);
-        KeyPair pair = keyGen.generateKeyPair();
-        this.privateKey = pair.getPrivate();
-        this.publicKey = pair.getPublic();
-    }
-
-    @NotNull
-    public PrivateKey getPrivateKey() {
-        return privateKey;
+    public RegistrationMessage() {
+        super();
     }
 
     @NotNull
     public PublicKey getPublicKey() {
         return publicKey;
+    }
+
+    public void setPublicKey(@NotNull PublicKey publicKey) {
+        this.publicKey = publicKey;
+    }
+
+    @NotNull
+    @Override
+    public String toString() {
+        return "REGISTRATION MESSAGE\n" + super.toString() + "\nPublic key: " + publicKey;
     }
 }
