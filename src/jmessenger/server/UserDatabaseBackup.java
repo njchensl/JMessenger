21c2bb68c6d66f3aaf39f106206aeaa8ff478a2c
nanjingchj <nanjingchj@gmail.com>
2019-12-27 09:14:38 +0800

    consistent line separators and code clean up

--- src/jmessenger/server/UserDatabaseBackup.java
+++ src/jmessenger/server/UserDatabaseBackup.java
@@ -1,3 +1,26 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2019 frche1699.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package jmessenger.server;
 
 import java.io.File;
@@ -14,6 +37,7 @@ public class UserDatabaseBackup implements Runnable {
     }
 
     @Override
+    @SuppressWarnings("ResultOfMethodCallIgnored")
     public void run() {
         File db = new File("database");
         if (!db.exists()) {
@@ -32,7 +56,7 @@ public class UserDatabaseBackup implements Runnable {
                 // recreate the file
                 db.delete();
                 db.createNewFile();
-                ObjectOutputStream out = null;
+                ObjectOutputStream out;
                 try {
                     out = new ObjectOutputStream(new FileOutputStream(db));
                 } catch (IOException e) {
